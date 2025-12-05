4d2bbd396381fd45e06df4e23787055ad7d9ef8c
Claudio Ghirelli Filho <claudioghirelli18@gmail.com>
2025-12-05 16:10:32 -0300

    Adicionar template de email na exibição de emails no admin root

--- app/Http/Controllers/AdminRoot/EmailPreviewController.php
+++ app/Http/Controllers/AdminRoot/EmailPreviewController.php
@@ -18,6 +18,11 @@ class EmailPreviewController extends Controller
                 'name' => 'Links de pagamento (Iugu)',
                 'description' => 'E-mail enviado aos clientes com o link da assinatura e cobrança de setup.',
             ],
+            [
+                'key' => 'iugu-charge',
+                'name' => 'Cobrança avulsa (Iugu)',
+                'description' => 'E-mail enviado com o link de pagamento de uma cobrança avulsa.',
+            ],
         ];
 
         return view('adminroot.emails.index', compact('templates'));
@@ -27,6 +32,7 @@ class EmailPreviewController extends Controller
     {
         return match ($template) {
             'iugu-payment-links' => $this->iuguPaymentLinksPreview(),
+            'iugu-charge' => $this->iuguChargePreview(),
             default => abort(404),
         };
     }
@@ -58,5 +64,27 @@ class EmailPreviewController extends Controller
         ];
 
         return view('emails.iugu.payment-links', compact('project', 'links'));
+    }
+
+    protected function iuguChargePreview(): View
+    {
+        $company = new Company([
+            'name' => 'Partners Corp',
+            'uri' => 'partners.example.com',
+            'brand_color' => '#4F46E5',
+            'logo_path' => null,
+        ]);
+
+        $customer = new Customer([
+            'name' => 'Cliente Exemplo',
+            'email' => 'cliente@example.com',
+        ]);
+
+        $customer->setRelation('company', $company);
+
+        $link = 'https://iugu.com/pay/cobranca123';
+        $emailMessage = 'Este é um exemplo de mensagem personalizada que acompanha a cobrança avulsa.';
+
+        return view('emails.iugu.charge', compact('customer', 'link', 'emailMessage'));
     }
 }
